e2a7905987de0f9c9f72aab10b63abf4bbf3c460
manisha069 <manisha.guptacs2018@indoreinstitute.com>
2021-03-07 15:51:01 +0530

    FSQRT

---
+++ FSQRT.py
@@ -0,0 +1,19 @@
+import math
+def isqrt(x):
+    if x < 0:
+        raise ValueError('square root not defined for negative numbers')
+    n = int(x)
+    if n == 0:
+        return 0
+    a, b = divmod(n.bit_length(), 2)
+    x = 2**(a+b)
+    while True:
+        y = (x + n//x)//2
+        if y >= x:
+            return x
+        x = y
+
+t=int(input())
+for _ in range(t):
+    x=int(input())
+    print(isqrt(x))
